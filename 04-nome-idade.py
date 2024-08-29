70f7244db766999a36d2db57111ee076da480857
marcosalves2009 <marcosalves020109@gmail.com>
2024-08-29 11:51:11 -0300

    Add files via upload

---
+++ 04-nome-idade.py
@@ -0,0 +1,24 @@
+# atribuição de variaveis
+nome = "marcos"
+idade = 15
+altura = 1.63
+
+# exibir mensagem na tela 
+print ("nome:", nome,  "idade", idade, "altura", altura)
+# formatação com f-string
+print (f"nome:, {nome}  idade, {idade} altura, {altura}")
+
+nome = input ("digite o seu nome:")
+idade = int (input  ("digite a sua idade:"))
+altura = float (input ("digite a sua altura:"))
+
+print (f"nome:, {nome}  idade, {idade} altura, {altura}")
+
+
+
+
+
+ 
+
+ 
+
