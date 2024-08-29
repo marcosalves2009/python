8954af9e49f73ac7caf63a0598f0c4791977a56f
marcosalves2009 <marcosalves020109@gmail.com>
2024-08-29 11:50:59 -0300

    Add files via upload

---
+++ 03-aula03.py
@@ -0,0 +1,16 @@
+# variaveis em python
+# não há necessidade de declará-las no inicio do programa
+# basta criar no meio do algoritimo com a atribuição
+
+teste = "Marcos"
+print (teste) # imprime a string "Marcos"
+print (type(teste)) # imprime o tipo da variavel, string
+teste = 10
+print (teste) # imprime o numero 10
+print (type (teste)) #imprime o tipo da variavel, que é int
+teste = 1.63
+print (teste) # imprime o numero 1.63
+print (type(teste)) # imprime o tipo da variavel, que é float
+teste = True 
+print (teste) # imprime o valor da true 
+print (type(teste)) # imprime o tipo da variavel, que é bool
