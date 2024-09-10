cd6a75ec460b5345640b73a942911f7fb0fcd8a4
marcosalves2009 <marcosalves020109@gmail.com>
2024-09-10 10:41:31 -0300

    Add files via upload

---
+++ 05-tipos-dados.py
@@ -0,0 +1,13 @@
+# tipos de dados em python
+# inteiros
+inteiro = 10
+print (inteiro)
+# floats
+float = 10.5
+print (float)
+# strings
+string = "Olá, mundo"
+print (string)
+# booleanos
+booleano = True
+print (booleano)
